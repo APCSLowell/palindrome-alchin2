b8f69ec971187630e49738f0dace3137a42b4d0f
alchin2 <142945840+alchin2@users.noreply.github.com>
2024-01-30 11:15:53 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,8 +37,7 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  if (word.equals(reverse(word))){return true;}
-  return false;
+  return (word.equals(reverse(word)));
 }
 public String reverse(String str)
 {
